88dd3e6f1f3e2e00c0d837eb8386bb5d9f9a02aa
Precious Olusola <preciousolusola16@gmail.com>
2023-06-22 16:22:59 +0100

    Implemented loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,76 @@
+'use client';
+import { useEffect } from 'react';
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from '@styles/page.module.css';
+
 
 export default function Home() {
+
+	useEffect(()=>{
+		(async ()=>{
+			// const res = await fetch('/api/members');
+			// const repo = await res.json()
+			// console.log(JSON.stringify(repo, null, 4));
+		})()
+	},[])
+
+
+	/* 
+		<a
+		href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+		className={styles.card}
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		<h2>
+			Docs <span>-&gt;</span>
+		</h2>
+
+		<p>
+			Find in-depth information about Next.js features and API.
+		</p>
+	</a>
+	*/
+
+
+	
 	return (
 		<main className={styles.main}>
 			<div className={styles.description}>
-				<p>
-				Get started by editing&nbsp;
-				<code className={styles.code}>app/page.tsx</code>
-				</p>
+				<h1>
+					Oila connect |&nbsp;
+					<a href="/" className={styles.nav}>
+						About
+					</a>
+				</h1>
+
 				<div>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					By{' '}
-					<Image
-					src="/vercel.svg"
-					alt="Vercel Logo"
-					className={styles.vercelLogo}
-					width={100}
-					height={24}
-					priority
-					/>
-				</a>
+					<span className={styles.credit}>
+						By{' '}
+						<h4>codepraycode</h4>
+					</span>
 				</div>
 			</div>
 
-			<div className={styles.center}>
-				<Image
-				className={styles.logo}
-				src="/next.svg"
-				alt="Next.js Logo"
-				width={180}
-				height={37}
-				priority
-				/>
-			</div>
-
 			<div className={styles.grid}>
-				<a
-				href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-				>
-				<h2>
-					Docs <span>-&gt;</span>
-				</h2>
-				<p>Find in-depth information about Next.js features and API.</p>
-				</a>
+				{
+					[1,2,3,4,5,6,7,8].map((e)=>(
+						<div
+						className={`${styles.cardd} ${styles.cardd_loading}`}
+						>							
+							<div></div>
 
-				<a
-				href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-				>
-				<h2>
-					Learn <span>-&gt;</span>
-				</h2>
-				<p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-				</a>
-
-				<a
-				href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-				>
-				<h2>
-					Templates <span>-&gt;</span>
-				</h2>
-				<p>Explore the Next.js 13 playground.</p>
-				</a>
-
-				<a
-				href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-				className={styles.card}
-				target="_blank"
-				rel="noopener noreferrer"
-				>
-				<h2>
-					Deploy <span>-&gt;</span>
-				</h2>
-				<p>
-					Instantly deploy your Next.js site to a shareable URL with Vercel.
-				</p>
-				</a>
+							<div className='profile-info'>
+								<span></span>
+								<span></span>
+								<div className={styles.bottom}>
+									<span></span>
+									<span></span>
+								</div>
+							</div>
+						</div>
+					))
+				}
 			</div>
 		</main>
 	)
