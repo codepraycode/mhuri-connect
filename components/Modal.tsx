4cbe1c416500e4fb34a932a10c560cd745d09e44
Precious Olusola <51521181+codepraycode@users.noreply.github.com>
2023-06-29 23:12:14 +0100

    Update Modal.tsx
    Added interface to modal reference

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,7 +9,7 @@ import { createPortal } from "react-dom";
 
 const Modal = ({children, open:isOpen, userCanClose, disableCloseOnBackgroundClick, onClose=()=>{}, }: ModalProps) =>{
 
-    const modal = useRef(null)
+    const modal = useRef<HTMLDialogElement>(null)
     useEffect(()=>{
         (()=>{
             // console.log(modal);
